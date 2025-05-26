e98a5b82e3b83b68bbbeba3ca6bfa79c640c95e0
Bastien Orivel <borivel@mozilla.com>
2025-05-26 13:19:23 +0200

    Fix equality to not raise exceptions when used with None or other types (#62)
    I've been writing too much rust and forgot that python's `__eq__` is
    called even when the type of `other` doesn't match ours.

--- test/test_eq.py
+++ test/test_eq.py
@@ -8,34 +8,48 @@ from mozilla_repo_urls import parse
     (
         pytest.param(
             "https://github.com/mozilla/repo.git",
-            "https://github.com/mozilla/repo.git",
+            parse("https://github.com/mozilla/repo.git"),
             True,
         ),
         pytest.param(
             "https://hg.mozilla.org/mozilla-central",
-            "https://hg.mozilla.org/mozilla-central",
+            parse("https://hg.mozilla.org/mozilla-central"),
             True,
         ),
         pytest.param(
             "https://github.com/mozilla/repo.git",
-            "https://github.com/mozilla/repo2.git",
+            parse("https://github.com/mozilla/repo2.git"),
             False,
         ),
         pytest.param(
             "https://github.com/mozilla/repo.git",
-            "https://github.com/mozilla2/repo.git",
+            parse("https://github.com/mozilla2/repo.git"),
             False,
         ),
         pytest.param(
             "https://hg.mozilla.org/mozilla-central",
-            "https://github.com/mozilla/repo.git",
+            parse("https://github.com/mozilla/repo.git"),
+            False,
+        ),
+        pytest.param(
+            "https://hg.mozilla.org/mozilla-central",
+            None,
+            False,
+        ),
+        pytest.param(
+            "https://hg.mozilla.org/mozilla-central",
+            "https://hg.mozilla.org/mozilla-central",  # This is a str on purpose
+            False,
+        ),
+        pytest.param(
+            "https://hg.mozilla.org/mozilla-central",
+            42,
             False,
         ),
     ),
 )
 def test_equality(left, right, should_be_equal):
     left = parse(left)
-    right = parse(right)
 
     if should_be_equal:
         assert left == right
